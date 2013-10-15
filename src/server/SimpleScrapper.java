dd959aac93ea0f7bf961dfadc0d489f2091b45f3
geezer <geezer@geezerok>
2013-10-15 12:33:34 +0300

    Minor fixes for first iteraton.

--- src/server/SimpleScrapper.java
+++ src/server/SimpleScrapper.java
@@ -1,9 +1,8 @@
 package server;
 
-import java.io.FileNotFoundException;
-
 import org.webharvest.definition.ScraperConfiguration;
 import org.webharvest.runtime.Scraper;
+import org.xml.sax.InputSource;
 
 
 public class SimpleScrapper {
@@ -11,14 +10,28 @@ public class SimpleScrapper {
 	
 	public static void main(String[] args){
 		try {
+		//TODO Move wizzairScrapperConfig.xml to classpath resourses
+			
+			InputSource in =  new InputSource(SimpleScrapper.class.
+					getClassLoader().getResourceAsStream("wizzairScrapperConfig.xml"));
+			
 			ScraperConfiguration config = 
-					new ScraperConfiguration("i:/MyJava/WizzAirScrapper/resources/wizzairScrapperConfig.xml");
+					new ScraperConfiguration(in);
+			
 			
 			scrapper = new Scraper(config, "/");
-			scrapper.setDebug(false);
+			scrapper.addVariableToContext("fromAirport", "IEV");
+			scrapper.addVariableToContext("toAirport", "GRO");
+			scrapper.addVariableToContext("fromDate", "10-11-2013");
+			scrapper.addVariableToContext("paxAdult", "1");
+			scrapper.addVariableToContext("paxChild", "0");
+			scrapper.addVariableToContext("fromInfant", "0");
+			scrapper.setDebug(true);
 			scrapper.execute();
-			System.out.println(scrapper.getContext().getVar("WAsearch"));
-		} catch (FileNotFoundException e) {
+
+			System.out.println("WizzAir club prices " + scrapper.getContext().getVar("pricesWAClub"));
+			System.out.println("No club prices " + scrapper.getContext().getVar("pricesNoClub"));
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
